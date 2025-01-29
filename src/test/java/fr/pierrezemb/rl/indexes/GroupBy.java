ca77fc7a15be322307af98121475deb0b52bed80
Pierre Zemb <pierre.zemb@clever-cloud.com>
2025-01-29 15:50:47 +0100

    bootstrap java

---
+++ src/test/java/fr/pierrezemb/rl/indexes/GroupBy.java
@@ -0,0 +1,13 @@
+package fr.pierrezemb.rl.indexes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GroupBy {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
