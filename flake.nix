1f54ac5b113ff87bfe26f84823ebe8427e757082
Pierre Zemb <pierre.zemb@clever-cloud.com>
2025-01-29 17:32:10 +0100

    add tests for groupBy index and query

--- flake.nix
+++ flake.nix
@@ -2,14 +2,20 @@
   description = "A Nix-flake-based Java development environment";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.fdb-overlay.url = "github:foundationdb-rs/overlay";
+  inputs.fdb-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, fdb-overlay }:
     let
       javaVersion = 22; # Change this value to update the whole stack
+      overlays = [ fdb-overlay.overlays.default ];
 
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit overlays;
+          inherit system;
+        };
       });
     in
     {
@@ -22,7 +28,8 @@
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ jdk maven gradle gcc ncurses patchelf zlib protobuf ];
+          packages = with pkgs; [ jdk maven gradle gcc ncurses patchelf zlib protobuf_25 libfdb73 ];
+          LD_LIBRARY_PATH = "${pkgs.libfdb73}/include";
         };
       });
     };
